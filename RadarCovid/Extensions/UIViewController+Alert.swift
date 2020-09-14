b116324955f585d8b634532c008f64a15386f3d3
alopezh <all.herranz@gmail.com>
2020-09-14 18:59:24 +0200

    refactor: centralize showAlert functionality

--- RadarCovid/Extensions/UIViewController+Alert.swift
+++ RadarCovid/Extensions/UIViewController+Alert.swift
@@ -19,6 +19,8 @@ protocol AlertController: class{
         message: String,
         buttonOkTitle: String,
         buttonCancelTitle: String,
+        buttonOkVoiceover: String?,
+        buttonCancelVoiceover: String?,
         okHandler: ((UIAlertAction) -> Void)?,
         cancelHandler: ((UIAlertAction) -> Void)?
     )
@@ -27,6 +29,8 @@ protocol AlertController: class{
         message: String,
         buttonOkTitle: String,
         buttonCancelTitle: String,
+        buttonOkVoiceover: String?,
+        buttonCancelVoiceover: String?,
         okHandler: ((UIAlertAction) -> Void)?
     )
 }
@@ -49,12 +53,33 @@ extension UIViewController : AlertController {
         self.view.showTransparentBackground(withColor: UIColor.blueyGrey90, alpha: 1)
         self.present(uiAlert, animated: true, completion: nil)
     }
+    
+    func showAlertOk(title: String, message: String, buttonTitle: String, buttonVoiceover: String? = nil, _ callback: ((Any) -> Void)? = nil) {
+
+        let uiAlert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        let action = UIAlertAction(title: buttonTitle, style: .default) { (alert) in
+            self.view.removeTransparentBackGround()
+            callback?(alert)
+        }
+        action.isAccessibilityElement = true
+        action.accessibilityLabel = buttonVoiceover
+        uiAlert.addAction(action)
+        let buttonView = uiAlert.view.subviews.first?.subviews.first?.subviews.first?.subviews[1]
+        uiAlert.view.tintColor = UIColor.white
+        buttonView?.backgroundColor  = #colorLiteral(red: 0.2, green: 0.1882352941, blue: 0.7254901961, alpha: 1)
+
+        self.view.showTransparentBackground(withColor: UIColor.blueyGrey90, alpha: 1)
+        self.present(uiAlert, animated: true, completion: nil)
+    }
 
     func showAlertCancelContinue(
         title: String,
         message: String,
         buttonOkTitle: String,
         buttonCancelTitle: String,
+        buttonOkVoiceover: String? = nil,
+        buttonCancelVoiceover: String? = nil,
         okHandler: ((UIAlertAction) -> Void)? = nil,
         cancelHandler: ((UIAlertAction) -> Void)? = nil
     ) {
@@ -65,12 +90,16 @@ extension UIViewController : AlertController {
             self.view.removeTransparentBackGround()
             okHandler?(action)
         }
+        action.isAccessibilityElement = true
+        action.accessibilityLabel = buttonOkVoiceover
         uiAlert.addAction(action)
 
         let actionCancel = UIAlertAction(title: buttonCancelTitle, style: .default) { (action) in
             self.view.removeTransparentBackGround()
             cancelHandler?(action)
         }
+        actionCancel.isAccessibilityElement = true
+        actionCancel.accessibilityLabel = buttonCancelVoiceover
         uiAlert.addAction(actionCancel)
         let buttonView = uiAlert.view.subviews.first?.subviews.first?.subviews.first?.subviews[1]
         uiAlert.view.tintColor = UIColor.white
@@ -86,6 +115,8 @@ extension UIViewController : AlertController {
         message: String,
         buttonOkTitle: String,
         buttonCancelTitle: String,
+        buttonOkVoiceover: String? = nil,
+        buttonCancelVoiceover: String? = nil,
         okHandler: ((UIAlertAction) -> Void)? = nil
     ) {
         showAlertCancelContinue(
@@ -93,6 +124,8 @@ extension UIViewController : AlertController {
             message: message,
             buttonOkTitle: buttonOkTitle,
             buttonCancelTitle: buttonCancelTitle,
+            buttonOkVoiceover: buttonOkVoiceover,
+            buttonCancelVoiceover: buttonCancelVoiceover,
             okHandler: okHandler,
             cancelHandler: nil
         )
