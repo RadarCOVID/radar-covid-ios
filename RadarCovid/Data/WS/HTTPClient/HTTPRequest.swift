ecff49c1e1acb0224fca520ab78431eb8d18b715
Jorge J. Ramos <jorge.ramos@me.com>
2020-09-15 09:48:49 +0200

    Prepare HTTPRequest to use default headers, without Alamofire references

--- RadarCovid/Data/WS/HTTPClient/HTTPRequest.swift
+++ RadarCovid/Data/WS/HTTPClient/HTTPRequest.swift
@@ -37,7 +37,8 @@ struct HTTPRequest<ResponseModel: Codable> {
         guard let baseURL = baseURL else { return }
         urlRequest = URLRequest(url: baseURL.appendingPathComponent(endpoint.address))
         urlRequest?.httpMethod = endpoint.method.rawValue
-        urlRequest?.allHTTPHeaderFields = headers
+        let allHeaders = headers.merging(defaultHTTPHeaders()) { (_, new) in new }
+        urlRequest?.allHTTPHeaderFields = allHeaders
     }
 
     private mutating func prepareParameters() {
@@ -53,5 +54,52 @@ struct HTTPRequest<ResponseModel: Codable> {
             let postData = try? JSONSerialization.data(withJSONObject: parameters, options: [])
             urlRequest?.httpBody = postData
         }
+    }
+
+    private func defaultHTTPHeaders() -> [String: String] {
+        var headersDictionary = [String: String]()
+        // Accept-Encoding HTTP Header; see https://tools.ietf.org/html/rfc7230#section-4.2.3
+        headersDictionary["Accept-Encoding"] = "gzip;q=1.0, compress;q=0.5"
+
+        // Accept-Language HTTP Header; see https://tools.ietf.org/html/rfc7231#section-5.3.5
+        let acceptLanguage = Locale.preferredLanguages.prefix(6).enumerated().map { index, languageCode in
+            let quality = 1.0 - (Double(index) * 0.1)
+            return "\(languageCode);q=\(quality)"
+        }.joined(separator: ", ")
+        headersDictionary["Accept-Language"] = acceptLanguage
+
+        // User-Agent Header; see https://tools.ietf.org/html/rfc7231#section-5.5.3
+        // Example: `iOS Example/1.0 (org.alamofire.iOS-Example; build:1; iOS 10.0.0) Alamofire/4.0.0`
+        if let info = Bundle.main.infoDictionary {
+            let executable = info[kCFBundleExecutableKey as String] as? String ?? "Unknown"
+            let bundle = info[kCFBundleIdentifierKey as String] as? String ?? "Unknown"
+            let appVersion = info["CFBundleShortVersionString"] as? String ?? "Unknown"
+            let appBuild = info[kCFBundleVersionKey as String] as? String ?? "Unknown"
+            let osNameVersion: String = {
+                let version = ProcessInfo.processInfo.operatingSystemVersion
+                let versionString = "\(version.majorVersion).\(version.minorVersion).\(version.patchVersion)"
+
+                let osName: String = {
+                    #if os(iOS)
+                        return "iOS"
+                    #elseif os(watchOS)
+                        return "watchOS"
+                    #elseif os(tvOS)
+                        return "tvOS"
+                    #elseif os(macOS)
+                        return "OS X"
+                    #elseif os(Linux)
+                        return "Linux"
+                    #else
+                        return "Unknown"
+                    #endif
+                }()
+
+                return "\(osName) \(versionString)"
+            }()
+
+            headersDictionary["Accept-Language"] = "\(executable)/\(appVersion) (\(bundle); build:\(appBuild); \(osNameVersion)) CFNetwork"
+        }
+        return headersDictionary
     }
 }
