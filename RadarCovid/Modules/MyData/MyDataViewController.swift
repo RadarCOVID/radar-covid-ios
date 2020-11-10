c4b5ea46c26c088d53fa78e34c2a93005e733744
Noel Carcases <ndcg91@yahoo.es>
2020-11-09 22:04:32 -0500

    chore: fix 28580

--- RadarCovid/Modules/MyData/MyDataViewController.swift
+++ RadarCovid/Modules/MyData/MyDataViewController.swift
@@ -19,6 +19,8 @@ class MyDataViewController: UIViewController {
     @IBOutlet weak var privacyLabel: UILabel!
     @IBOutlet weak var bullet2Label: UILabel!
     @IBOutlet weak var bullet3Label: UILabel!
+    @IBOutlet weak var viñeta1: UILabel!
+    @IBOutlet weak var viñeta2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,6 +65,8 @@ class MyDataViewController: UIViewController {
         privacyLabel.accessibilityHint = "ACC_HINT".localized
         
         if UIAccessibility.isVoiceOverRunning {
+            viñeta1.isHidden = true
+            viñeta2.isHidden = true
             descriptionLabel.text = descriptionLabel.text?.lowercased()
             bullet2Label.text = bullet2Label.text?.lowercased()
             bullet3Label.text = bullet3Label.text?.lowercased()
