f22e06b89333d86f5c6cb83b0e6c2e8c177b6a64
alopezh <all.herranz@gmail.com>
2021-02-09 21:20:06 +0100

    chore: revert lost X-EFGS-Sharing header

--- RadarCovid/Data/WS/Verification/APIs/VerificationControllerAPI.swift
+++ RadarCovid/Data/WS/Verification/APIs/VerificationControllerAPI.swift
@@ -28,7 +28,8 @@ open class VerificationControllerAPI {
      - parameter completion: completion handler to receive the data and the error objects
      */
     open func verifyCode(body: Code, share: Bool, completion: @escaping ((_ data: TokenResponse?, _ error: Error?) -> Void)) {
-        verifyCodeWithRequestBuilder(body: body).execute { (response, error) -> Void in
+        let headers = ["X-EFGS-Sharing": share ? "1" : "0"]
+        verifyCodeWithRequestBuilder(body: body, headers: headers).execute { (response, error) -> Void in
             completion(response?.body, error)
         }
     }
