d3394b8034399d9d46d0a3907587fa95b3a2a47d
alopezh <all.herranz@gmail.com>
2021-05-04 16:21:06 +0200

    chore: fix  30654 read badge in voiceover

--- RadarCovid/Modules/TabBarController.swift
+++ RadarCovid/Modules/TabBarController.swift
@@ -155,9 +155,15 @@ class TabBarController: UITabBarController {
     }
     
     private func loadBadges() {
-        venueRecodrUseCase.isCheckedIn().subscribe { [weak self] checked in
-            self?.venueRecordViewController.tabBarItem.badgeValue = checked ? "" : nil
-        }.disposed(by: disposeBag)
+        venueRecodrUseCase.isCheckedIn().subscribe ( onNext: { [weak self] checked in
+            if checked {
+                self?.venueRecordViewController.tabBarItem.badgeValue = ""
+                self?.venueRecordViewController.tabBarItem.accessibilityLabel = "ACC_VENUE_TITLE".localized + ". " + "VENUE_RECORD_CHECKIN_TITLE".localized
+            } else {
+                self?.venueRecordViewController.tabBarItem.badgeValue = nil
+                self?.venueRecordViewController.tabBarItem.accessibilityLabel = "ACC_VENUE_TITLE".localized
+            }
+        }).disposed(by: disposeBag)
     }
     
     override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
